97935f85e03896168073dc9a1a8f90d1d528cc5f
Mohamed Toure <demahom.toure@gmail.com>
2022-03-05 22:58:05 +0100

    change server.js content

--- server.js
+++ server.js
@@ -9,10 +9,10 @@ const express = require('express');
 const app = express();
 
 app.use(requireHTTPS);
-app.use(express.static('./dist/tourem-ui'));
+app.use(express.static('./dist/<name-on-package.json>'));
 
 app.get('/*', function(req, res) {
-  res.sendFile('index.html', {root: 'dist/tourem-ui/'});
+  res.sendFile('index.html', {root: 'dist/<name-on-package.json>/'});
 });
 
 app.listen(process.env.PORT || 8080);
