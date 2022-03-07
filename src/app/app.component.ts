2b6ece2f631a70f6cbc78ba784e34d7cd5500b33
Mohamed Toure <demahom.toure@gmail.com>
2022-03-08 00:04:00 +0100

    add menu animation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {ResponseData} from "./model/ResponseData";
 import {HttpClient} from "@angular/common/http";
-import {Data} from "./model/Data";
 import {Article} from "./model/Article";
 
 @Component({
@@ -11,16 +10,20 @@ import {Article} from "./model/Article";
 })
 export class AppComponent implements OnInit {
   articles: Article[] | undefined ;
+  menuOpened = false;
 
   constructor(private http: HttpClient) {}
 
   ngOnInit(): void {}
 
   findArticles(): void {
-    console.log("Getting articles..");
     this.http.get<ResponseData>("https://tourem-test.herokuapp.com/api/articles", {responseType: "json"}).subscribe(res => {
       console.log(res);
       this.articles = res.data.content;
     });
+  }
+
+  onMenuEventReceived(event: string) {
+    this.menuOpened = !this.menuOpened;
   }
 }
--- src/app/pages/header/header.component.ts
+++ src/app/pages/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  @Output() menuEventEmitter = new EventEmitter<any>();
+
+
+
+  constructor() {}
 
   ngOnInit(): void {
+  }
+
+  onMenuClick(): void {
+    this.menuEventEmitter.emit("click");
   }
 
 }
